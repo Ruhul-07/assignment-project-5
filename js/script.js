00ded6fcef0f4b6d911e743a2ff2b94a771fbf31
Ruhul Amin <ruhulaminbabu93@gmail.com>
2024-09-23 19:29:26 +0600

    danate now button fuction added and main balance also interactived

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ function getInputValueById(id){
 document.getElementById('history-btn').addEventListener('click', function(){
     const historyBtn = document.getElementById('history-btn');
     const donationBtn = document.getElementById('donation-btn');
-
     historyBtn.classList.add('bg-primary')
     donationBtn.classList.remove('bg-primary')
 
@@ -25,7 +24,6 @@ document.getElementById('history-btn').addEventListener('click', function(){
 document.getElementById('donation-btn').addEventListener('click', function(){
     const historyBtn = document.getElementById('history-btn');
     const donationBtn = document.getElementById('donation-btn');
-
     historyBtn.classList.remove('bg-primary')
     donationBtn.classList.add('bg-primary')
 
@@ -34,11 +32,58 @@ document.getElementById('donation-btn').addEventListener('click', function(){
    card.classList.remove('hidden')
 })
 
-// Donate Now Button Function
 
-document.getElementById('donate-now').addEventListener('click', function(){
-    const inputValue = getInputValueById('input-value');
-    
+// Donate Now Button Section 1 Function
+document.getElementById('donate-now-1').addEventListener('click', function(){
+    const showAmountElement = document.getElementById('show-amount-1');
+    const showAmount = parseFloat(showAmountElement.innerText);
+    const mainBalanceElement = document.getElementById('main-balance');
+    const mainBalance = parseFloat(mainBalanceElement.innerText);
 
+    const inputValue = getInputValueById('input-value-1');
+    const newShowAmount = showAmount + inputValue;
+
+    showAmountElement.innerText = newShowAmount;
+
+    const remainingBalance = mainBalance - inputValue;
+    mainBalanceElement.innerText = remainingBalance;
+      
+})
+
+// Donate Now Button Section 2 Function
+
+document.getElementById('donate-now-2').addEventListener('click', function(){
+    const showAmountElement = document.getElementById('show-amount-2');
+    const showAmount = parseFloat(showAmountElement.innerText);
+
+    const mainBalanceElement = document.getElementById('main-balance');
+    const mainBalance = parseFloat(mainBalanceElement.innerText);
+
+    const inputValue = getInputValueById('input-value-2');
+    const newShowAmount = showAmount + inputValue;
+
+    showAmountElement.innerText = newShowAmount;
+
+    const remainingBalance = mainBalance - inputValue;
+    mainBalanceElement.innerText = remainingBalance;
+      
+})
+
+
+// Donate Now Button Section 3 Function
+
+document.getElementById('donate-now-3').addEventListener('click', function(){
+    const showAmountElement = document.getElementById('show-amount-3');
+    const showAmount = parseFloat(showAmountElement.innerText);
+
+    const mainBalanceElement = document.getElementById('main-balance');
+    const mainBalance = parseFloat(mainBalanceElement.innerText);
+
+    const inputValue = getInputValueById('input-value-3');
+    const newShowAmount = showAmount + inputValue;
+
+    showAmountElement.innerText = newShowAmount;  
     
+    const remainingBalance = mainBalance - inputValue;
+    mainBalanceElement.innerText = remainingBalance;
 })
