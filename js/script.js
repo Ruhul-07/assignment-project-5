007e0b7b46ad3d3c00e1f1e44d45677e85b6ceac
Ruhul Amin <ruhulaminbabu93@gmail.com>
2024-09-23 23:19:23 +0600

    history show added with common function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 
-
-//common function
+/**------------------Common Function---------------------- */
 
 function getInputValueById(id){
     const inputValue = parseFloat(document.getElementById(id).value);
     return inputValue;
 
 }
+// History show Function
 
+function history(id){
+    const inputValue = getInputValueById(id)
+    const historyItem = document.createElement('div');
+    historyItem.className = "rounded-lg p-8 border-1 border-bg-[#1111111]";
+    historyItem.innerHTML = `
+        <h4 class="font-bold text-[#111111] text-xl">${inputValue} Taka is donated for flood-2024 at Noakhali,Bangladesh</h4>
+        <p class="text-base text-[#111111B3] font-light">Date:${new Date()}</p>
+    `;
+
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
+    return historyContainer;
+}
 
 // History Button Function
+
 document.getElementById('history-btn').addEventListener('click', function(){
     const historyBtn = document.getElementById('history-btn');
     const donationBtn = document.getElementById('donation-btn');
@@ -19,6 +34,8 @@ document.getElementById('history-btn').addEventListener('click', function(){
    const card = document.getElementById('card');
 
    card.classList.add('hidden')
+
+
 })
 
 document.getElementById('donation-btn').addEventListener('click', function(){
@@ -47,7 +64,7 @@ document.getElementById('donate-now-1').addEventListener('click', function(){
 
     const remainingBalance = mainBalance - inputValue;
     mainBalanceElement.innerText = remainingBalance;
-      
+    history('input-value-1');
 })
 
 // Donate Now Button Section 2 Function
@@ -66,7 +83,7 @@ document.getElementById('donate-now-2').addEventListener('click', function(){
 
     const remainingBalance = mainBalance - inputValue;
     mainBalanceElement.innerText = remainingBalance;
-      
+    history('input-value-2');  
 })
 
 
@@ -86,4 +103,6 @@ document.getElementById('donate-now-3').addEventListener('click', function(){
     
     const remainingBalance = mainBalance - inputValue;
     mainBalanceElement.innerText = remainingBalance;
+    history('input-value-3');
 })
+
